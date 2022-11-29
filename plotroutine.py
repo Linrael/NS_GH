64ec5b800c43a482f472091a3b609c0daf3ca442
Apoll1on <aseeliger@me.com>
2022-11-29 12:07:07 +0100

    Geklappt

--- plotroutine.py
+++ plotroutine.py
@@ -12,11 +12,11 @@ print(imax,jmax,xlength,ylength,sep=" ")
 
 U=f.readline().split('/')[:-1]
 U=np.array(U)
-U=U.astype(np.double).reshape(imax+2,jmax+2)
+U=100*U.astype(np.double).reshape(imax+2,jmax+2)
 
 V=f.readline().split('/')[:-1]
 V=np.array(V)
-V=V.astype(np.double).reshape(imax+2,jmax+2)
+V=100*V.astype(np.double).reshape(imax+2,jmax+2)
 
 P=f.readline().split('/')[:-1]
 P=np.array(P)
@@ -26,6 +26,9 @@ X=np.arange(0,xlength+1.1*xlength/(imax),xlength/(imax))
 Y=np.arange(0,ylength+1.1*ylength/(jmax),ylength/(jmax))
 X,Y=np.meshgrid(X,Y)
 
+fig,ax=plt.subplots()
+ax.streamplot(X,Y,U,V,color=U, linewidth=2, cmap='autumn')
+plt.show()
 
 fig,ax= plt.subplots()
 ax.quiver(X,Y,U,V)
