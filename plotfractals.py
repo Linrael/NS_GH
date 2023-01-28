072ced5289c54e27729f849abde86a3a3e796dc7
Linrael <jpgrandm@gmail.com>
2023-01-28 19:32:40 +0100

    plotfractals works

--- plotfractals.py
+++ plotfractals.py
@@ -43,54 +43,59 @@ konvertieren[malen > 0.5] = [1, 1, 1]
 konvertieren[malen < 0.5] = [0, 0, 0]
 konvertieren = konvertieren[::-1, :]
 
-malfig, malax = plt.subplots()
-malax.imshow(konvertieren, extent=[0, 3, 0, 0.5], interpolation='nearest')
-malax.set_title("Geometry")
-plt.show()
+# malfig, malax = plt.subplots()
+# malax.imshow(konvertieren, extent=[0, 3, 0, 0.5], interpolation='nearest')
+# malax.set_title("Geometry")
+# plt.show()
 
 fluid_flag = np.logical_not(fluid_flag)
 
-startx=0.2
-endx=0.45
-starty=0.
-endy=0.5
+startx = 1.75
+endx = 1.8
+starty = 0.1
+endy = 0.12
 
 X = np.arange(0, xlength + 0 * 1.1 * xlength / imax, xlength / imax)
 Y = np.arange(0, ylength + 0 * 1.1 * ylength / jmax, ylength / jmax)
 X, Y = np.meshgrid(X, Y)
-Xsmall=X[int(startx*imax):int(endx*imax), int(starty*jmax):int(endy*jmax)]
-Ysmall = Y[int(startx*imax):int(endx*imax), int(starty*jmax):int(endy*jmax)]
+# Xsmall2 = X[int(startx * imax):int(endx * imax), int(starty * jmax):int(endy * jmax)]
+# Ysmall = Y[int(startx * imax):int(endx * imax), int(starty * jmax):int(endy * jmax)]
+# print(Xsmall2.shape)
+print(Y.shape)
+
+Xsmall = np.arange(startx, endx, xlength/imax)
+Ysmall = np.arange(starty, endy, ylength/jmax)
+Xsmall, Ysmall = np.meshgrid(Xsmall, Ysmall)
+print(Ysmall.shape)
 # now read additional data
 
-
-
+tz = 0
 while True:
     line = f.readline()
+    # if tz == 2:
+    #     break
     if not line:
         break
     timestep = int(line)
-    U = np.swapaxes(np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax + 2, jmax + 2), 0, 1)[1:-1,:]
-    V = np.swapaxes(np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax + 2, jmax + 2), 0, 1)[1:-1,:]
+    U = np.swapaxes(np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax + 2, jmax + 2), 0, 1)
+    V = np.swapaxes(np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax + 2, jmax + 2), 0, 1)
     P = np.swapaxes(np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax + 2, jmax + 2), 0, 1)
+    # choose choatic part:
+    U = U[int(starty / ylength * jmax) + 1: int(endy / ylength * jmax) + 1, int(startx / xlength * imax) + 1: int(endx / xlength * imax) + 2]
+    V = V[int(starty / ylength * jmax) + 1: int(endy / ylength * jmax) + 1, int(startx / xlength * imax) + 1: int(endx / xlength * imax) + 2]
+    # maskedP = np.ma.array(P, mask=fluid_flag)
+    # maskedP = maskedP[1:-1, :]
+    # maskedP = maskedP[int(starty / ylength * jmax) + 1: int(endy / ylength * jmax) + 1, int(startx / xlength * imax) + 1: int(endx / xlength * imax) + 1]
+    # maskedP = maskedP[::-1, :]
+    #
+    print(V.shape)
 
-    #choose choatic part:
-    U = U[int(startx*imax):int(endx*imax), int(starty*jmax)+1:int(endy*jmax)+1]
-    V = V[int(startx*imax):int(endx*imax), int(starty*jmax)+1:int(endy*jmax)+1]
-    maskedP = np.ma.array(P, mask=fluid_flag)
-    maskedP=maskedP[1:-1,:]
-    maskedP = maskedP[int(startx*imax):int(endx*imax), int(starty*jmax)+1:int(endy*jmax)+1]
-    maskedP = maskedP[::-1, :]
-
-    fig, ax = plt.subplots(1,figsize=(15,10))
+    fig, ax = plt.subplots(1)
     ax.streamplot(Xsmall, Ysmall, U, V, color=U, linewidth=0.5, cmap='autumn')
     ax.set_title(f'U V Stream Plot at Timestep {timestep} for time# {timestep * delt}')
-    ax.imshow(konvertieren, extent=[startx*xlength, endx*xlength, starty*ylength, endy*ylength], interpolation='nearest')
-    ax.imshow(maskedP, extent=[startx*xlength, endx*xlength, starty*ylength, endy*ylength])
+    # ax.imshow(konvertieren, extent=[startx * xlength, endx * xlength, starty * ylength, endy * ylength], interpolation='nearest')
+    # ax.imshow(maskedP, extent=[startx, endx, starty, endy])
     plt.show()
+    tz += 1
 
-
-
-
-
-
-    #11.036 seconds is last data for bs
+    # 11.036 seconds is last data for bs
