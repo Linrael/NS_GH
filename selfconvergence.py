29f3e348ae3606857dda2ae31897f3188a6d84d1
Programmieren <a.seeliger@uni-jena.de>
2023-01-29 15:21:15 +0100

    220123

--- selfconvergence.py
+++ selfconvergence.py
@@ -18,6 +18,7 @@ timesteps50 = int(seconds / delt50 / skipsteps) + 1
 print(timesteps50)
 u50 = np.zeros((timesteps50, imax50, jmax50), dtype=np.double)
 v50 = np.zeros_like(u50)
+p50 = np.zeros_like(u50)
 timesteps_array50 = np.zeros((timesteps50))
 for i in range(4):
     f.readline()
@@ -29,7 +30,7 @@ for i in range(timesteps50):
     timesteps_array50[i] = int(line)
     u50[i] = np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax50 + 2, jmax50 + 2)[1:-1, 1:-1]
     v50[i] = np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax50 + 2, jmax50 + 2)[1:-1, 1:-1]
-    f.readline()
+    p50[i] = np.array(f.readline().split('/')[:-1]).astype(np.double).reshape(imax50 + 2, jmax50 + 2)[1:-1, 1:-1]
 
 f.close()
 
@@ -45,6 +46,7 @@ timesteps100 = int(seconds / delt100/ skipsteps) + 1
 print(timesteps100)
 u100 = np.zeros((timesteps100, imax100, jmax100), dtype=np.double)
 v100 = np.zeros_like(u100)
+p100 = np.zeros_like(u100)
 timesteps_array100 = np.zeros((timesteps100))
 for i in range(4):
     g.readline()
@@ -56,7 +58,7 @@ for i in range(timesteps100):
     timesteps_array100[i] = int(line)
     u100[i] = np.array(g.readline().split('/')[:-1]).astype(np.double).reshape(imax100 + 2, jmax100 + 2)[1:-1, 1:-1]
     v100[i] = np.array(g.readline().split('/')[:-1]).astype(np.double).reshape(imax100 + 2, jmax100 + 2)[1:-1, 1:-1]
-    g.readline()
+    p100[i] = np.array(g.readline().split('/')[:-1]).astype(np.double).reshape(imax100 + 2, jmax100 + 2)[1:-1, 1:-1]
 
 g.close()
 
@@ -72,6 +74,7 @@ timesteps200 = int(seconds / delt200/ skipsteps) + 1
 print(timesteps200)
 u200 = np.zeros((timesteps200, imax200, jmax200), dtype=np.double)
 v200 = np.zeros_like(u200)
+p200 = np.zeros_like(u200)
 timesteps_array200 = np.zeros((timesteps200))
 for i in range(4):
     h.readline()
@@ -83,7 +86,7 @@ for i in range(timesteps200):
     timesteps_array200[i] = int(line)
     u200[i] = np.array(h.readline().split('/')[:-1]).astype(np.double).reshape(imax200 + 2, jmax200 + 2)[1:-1, 1:-1]
     v200[i] = np.array(h.readline().split('/')[:-1]).astype(np.double).reshape(imax200 + 2, jmax200 + 2)[1:-1, 1:-1]
-    h.readline()
+    p200[i] = np.array(h.readline().split('/')[:-1]).astype(np.double).reshape(imax200 + 2, jmax200 + 2)[1:-1, 1:-1]
 
 h.close()
 
@@ -118,5 +121,6 @@ ax.plot((timesteps_array50 * delt50)[1:], np.log2(qvaluep),color="red",linewidth
 ax.set_title("Self-Convergence Test")
 ax.set_xlabel("Time t in seconds")
 ax.set_ylabel("Convergence order p")
+ax.legend()
 
 plt.show()
